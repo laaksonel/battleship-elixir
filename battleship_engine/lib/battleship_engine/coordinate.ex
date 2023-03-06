e718281703a0c6372430bbf27b1ad84df4aff35e
Lauri Laaksonen <laaksonel@gmail.com>
2023-03-06 18:23:38 +0200

    Missing coordinate functions

--- battleship_engine/lib/battleship_engine/coordinate.ex
+++ battleship_engine/lib/battleship_engine/coordinate.ex
@@ -18,11 +18,19 @@ defmodule BattleshipEngine.Coordinate do
     end
   end
 
+  def guess(coordinate) do
+    Agent.get(coordinate, fn coordinate -> %{coordinate | guessed?: true} end)
+  end
+
   def guessed?(coordinate) do
     Agent.get(coordinate, fn state -> state.guessed? end)
   end
 
   def hit?(coordinate) do
     in_ship(coordinate) && guessed?(coordinate)
+  end
+
+  def set_in_ship(coordinate, ship) do
+    Agent.update(coordinate, fn state -> %{state | in_ship: ship} end)
   end
 end
