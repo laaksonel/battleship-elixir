0ac2c4f7a5fd435669d674806a2a64f1f4066919
Lauri Laaksonen <laaksonel@gmail.com>
2023-03-11 15:49:52 +0200

    Fix wrong arity

--- battleship_engine/lib/battleship_engine/game_supervisor.ex
+++ battleship_engine/lib/battleship_engine/game_supervisor.ex
@@ -1,7 +1,13 @@
 defmodule BattleshipEngine.GameSupervisor do
+  alias BattleshipEngine.Game
   use DynamicSupervisor
 
-  def start_link() do
+  def start_game(name) do
+    spec = %{id: Game, start: {Game, :start_link, [name]} }
+    DynamicSupervisor.start_child(__MODULE__, spec)
+  end
+
+  def start_link(_args) do
     DynamicSupervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
