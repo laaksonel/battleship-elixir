8c7f58f2551419fea1917d9483a003f1b2558623
Lauri Laaksonen <laaksonel@gmail.com>
2023-03-11 16:10:54 +0200

    Create child spec via GenServer

--- battleship_engine/lib/battleship_engine/game_supervisor.ex
+++ battleship_engine/lib/battleship_engine/game_supervisor.ex
@@ -3,7 +3,9 @@ defmodule BattleshipEngine.GameSupervisor do
   use DynamicSupervisor
 
   def start_game(name) do
-    DynamicSupervisor.start_child(__MODULE__, {Game, [name]})
+    DynamicSupervisor.start_child(__MODULE__, Game.child_spec(name))
+  end
+
   def stop_game(name) do
     DynamicSupervisor.terminate_child(__MODULE__, pid_from_name(name))
   end
